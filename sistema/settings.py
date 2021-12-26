df8a56b51258206c1eb19538aab06f24cac5f5a7
lucasmarrtt <lucasschnneider009@gmail.com>
2021-12-26 11:21:29 -0300

    colocando no Heroku

--- sistema/settings.py
+++ sistema/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Apagar depois 
-import dj_static
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
